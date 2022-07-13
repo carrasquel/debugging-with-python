1e61778e59096418491135b88c21d6814425097f
Nelson Carrasquel <carrsquel@outlook.com>
2022-07-13 01:58:17 -0400

    pdb example added

---
+++ add_numbers.py
@@ -0,0 +1,10 @@
+print("Enter first number to add: ")
+first = input()
+print("Enter second number to add: ")
+second = input()
+breakpoint()
+print("Enter third number to add: ")
+third = input()
+
+print(f'The sum is {first + second + third}')
+
